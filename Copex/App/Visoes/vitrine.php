a3eda230fb7eacdc7a0e01528b7a725fcedf3d1f
Adriano Lima <analista.adrianolima@gmail.com>
2018-02-14 18:28:58 -0300

    Alterações na vitrine
    Alterações na vitrine

--- Copex/App/Visoes/vitrine.php
+++ Copex/App/Visoes/vitrine.php
@@ -1,6 +1,6 @@
 <main>
     <div class="row">
-        <div class="col s6 offset-s3 center">
+        <div class="col s12 offset-s0 center">
             <img style="width: 100%" src="img/img_padrao.png">
         </div>
     </div>
